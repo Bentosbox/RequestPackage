cbdf443ae8fc1f5a1ed6d33d1d6de01d1655f1da
Ben Li <ben_li5@yahoo.ca>
2017-03-13 21:32:12 +0000

    added download messages

--- request.js
+++ request.js
@@ -1,11 +1,17 @@
 var request = require('request');
 var fs = require('fs');
-
+console.log('Downloading ...');
 request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
        .on('error', function (err) {                                   // Note 2
          throw err;
        })
-       .on('response', function (response) {                           // Note 3
-         console.log('Response Status Code: ', response.Message, response.headers['content-type']);
+       .on('response', function (response) {
+        // console.log('Response Status Code: ', response.statusCode);                           // Note 3
+        console.log('Response Status Code: ', response.statusCode);
+        console.log('Content Type: ', response.headers['content-type']);
+       })
+       .on('end', function(end) {
+        console.log('Download Completed');
        })
        .pipe(fs.createWriteStream('./future.jpg'));
+
